cb9a3afc08f85f87c1df5b615ce7ffb7cdce5f10
Tetiana <Tatyana.granchenko@gmail.com>
2019-06-06 19:34:04 +0300

    aded query parameters and models

---
+++ Les_11_Networking/Models/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  Les_11_Networking
+//
+//  Created by Tetiana Hranchenko on 6/5/19.
+//  Copyright © 2019 Canux Corporation. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    var userId: Int?
+    var id: Int?
+    var title: String?
+    var body: String?
+    
+}
---
+++ Les_11_Networking/Models/UserModels.swift
@@ -0,0 +1,9 @@
+//
+//  Users.swift
+//  Les_11_Networking
+//
+//  Created by Tetiana Hranchenko on 6/5/19.
+//  Copyright © 2019 Canux Corporation. All rights reserved.
+//
+
+import Foundation
